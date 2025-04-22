ee140bb45b8f4e91fd63139e63ccefa182dc24e1
Mert Koçak <74009802+MertKocak@users.noreply.github.com>
2025-04-22 15:53:59 +0300

    Update Main.java

--- KDVTutariHesaplayanProgram/src/Main.java
+++ KDVTutariHesaplayanProgram/src/Main.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double fiyat;
+        double fiyat, kdvoran = 0;
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Fiyatı giriniz: ");
         fiyat = sc.nextDouble();
 
-        System.out.println("KDV'li fiyat: " + (fiyat + (fiyat * 18 / 100)));
-        System.out.println("KDV miktarı: " + (fiyat * 18 / 100));
+        if (fiyat <= 1000 && fiyat > 0) {
+            kdvoran = 0.18;
+        } else if (fiyat > 1000) {
+            kdvoran = 0.08;
+        } else {
+            System.out.println("Girdiğiniz değer hatalı.");
+        }
+
+        System.out.println("KDV'li fiyat: " + (fiyat + (fiyat * kdvoran)));
+        System.out.println("KDV miktarı: " + kdvoran);
 
     }
 }
