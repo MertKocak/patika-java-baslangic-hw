d2df18fbbaeb316e32f3aa61d587d5299c97a6c0
Mert Koçak <74009802+MertKocak@users.noreply.github.com>
2025-04-24 14:56:34 +0300

    Update Main.java

--- HesapMakinesi/src/Main.java
+++ HesapMakinesi/src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double sayi1=0;
-        double sayi2=0;
+        double sayi1 = 0;
+        double sayi2 = 0;
         int islem = 0;
 
         Scanner sc = new Scanner(System.in);
@@ -18,27 +18,26 @@ public class Main {
         System.out.println("1-toplama, 2-çıkarma, 3-çarpma, 4-bölme • işlemi seçiniz: ");
         islem = sc.nextInt();
 
-        if(islem == 1){
-            System.out.println("sonuc: " + (sayi1 + sayi2));
+        switch (islem) {
+            case 1:
+                System.out.println("sonuc: " + (sayi1 + sayi2));
+                break;
+            case 2:
+                System.out.println("sonuc: " + (sayi1 - sayi2));
+                break;
+            case 3:
+                System.out.println("sonuc: " + (sayi1 * sayi2));
+                break;
+            case 4:
+                if (sayi2 != 0) {
+                    System.out.println("sonuc: " + (sayi1 / sayi2));
+                } else {
+                    System.out.println("bir sayı 0'a bölünemez!");
+                }
+                break;
+            default:
+                System.out.println("yanlış seçim yaptınız");
+                break;
         }
-        else if(islem == 2){
-            System.out.println("sonuc: " + (sayi1 - sayi2));
-        }
-        else if(islem == 3){
-            System.out.println("sonuc: " + (sayi1 * sayi2));
-        }
-        else if(islem == 4){
-            if(sayi2 != 0){
-                System.out.println("sonuc: " + (sayi1 / sayi2));
-            }
-            else{
-                System.out.println("bir sayı 0'a bölünemez!");
-            }
-        } else {
-            System.out.println("yanlış seçim yaptınız");
-        }
-
-
-
     }
 }
